120437c7f8909033399dfddcce737b5ea2c1353e
qiran51 <liwanjing5_1@163.com>
2022-01-21 00:26:40 +0800

    新增获取Cookies类

--- Chapter9/src/main/java/cn/liam/httpclient/cookies/MyCookiesForGet.java
+++ Chapter9/src/main/java/cn/liam/httpclient/cookies/MyCookiesForGet.java
@@ -1,7 +1,10 @@
 package cn.liam.httpclient.cookies;
 
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -9,12 +12,14 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class MyCookiesForGet {
     private String url;
     private ResourceBundle bundle;
+    private CloseableHttpClient client;
 
     @BeforeTest
     public void beforeTest(){
@@ -27,9 +32,34 @@ public class MyCookiesForGet {
         //从配置文件中，拼接测试的url
         HttpGet get = new HttpGet(this.url+bundle.getString("getCookies.uri"));
         //测试get请求
-        CloseableHttpClient client = HttpClients.createDefault();
+        //CloseableHttpClient client = HttpClients.createDefault();
+        CookieStore cookieStore = new BasicCookieStore();
+        //获取Cookies信息
+        client = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
         CloseableHttpResponse response = client.execute(get);
         result = EntityUtils.toString(response.getEntity(),"utf-8");
         System.out.println(result);
+
+        //获取Cookies信息
+        //获取CookiesStore对象
+        List<Cookie> cookieList = cookieStore.getCookies();
+        //循环输出ListCookies对象
+        for(Cookie cookie : cookieList){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            //逐步迭代
+            System.out.println("Cookie name="+name+";Cookie value="+value);
+        }
     }
+    //依赖testGetCookies类
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testGetWithCookies(){
+        //拼接测试的url
+        HttpGet get = new HttpGet(this.url+bundle.getString("test.get.with.cookies"));
+        CloseableHttpClient client = HttpClients.createDefault();
+
+        //设置Cookie信息
+
+    }
+
 }
