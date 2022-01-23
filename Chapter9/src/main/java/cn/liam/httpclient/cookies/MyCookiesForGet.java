58c678ba1ce3062d77130e0c6df96da528a1dfea
qiran51 <liwanjing5_1@163.com>
2022-01-23 23:47:27 +0800

    新增获取Cookies类

--- Chapter9/src/main/java/cn/liam/httpclient/cookies/MyCookiesForGet.java
+++ Chapter9/src/main/java/cn/liam/httpclient/cookies/MyCookiesForGet.java
@@ -19,8 +19,7 @@ import java.util.ResourceBundle;
 public class MyCookiesForGet {
     private String url;
     private ResourceBundle bundle;
-    private CloseableHttpClient client;
-
+    private CookieStore cookieStore;
     @BeforeTest
     public void beforeTest(){
         bundle = ResourceBundle.getBundle("application", Locale.CHINA);
@@ -33,17 +32,19 @@ public class MyCookiesForGet {
         HttpGet get = new HttpGet(this.url+bundle.getString("getCookies.uri"));
         //测试get请求
         //CloseableHttpClient client = HttpClients.createDefault();
+        //创建CookieStore对象
         CookieStore cookieStore = new BasicCookieStore();
-        //获取Cookies信息
-        client = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
+        //创建CloseableHttpClient对象，同时设置CookieStore
+        CloseableHttpClient client = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
+        //创建Response对象执行get方法
         CloseableHttpResponse response = client.execute(get);
+        //获得响应数据指定UTF-8
         result = EntityUtils.toString(response.getEntity(),"utf-8");
         System.out.println(result);
-
-        //获取Cookies信息
-        //获取CookiesStore对象
-        List<Cookie> cookieList = cookieStore.getCookies();
+        //获取CookieStore,为全局变量cookieStore赋值
+        this.cookieStore = cookieStore;
         //循环输出ListCookies对象
+        List<Cookie> cookieList = cookieStore.getCookies();
         for(Cookie cookie : cookieList){
             String name = cookie.getName();
             String value = cookie.getValue();
@@ -53,12 +54,25 @@ public class MyCookiesForGet {
     }
     //依赖testGetCookies类
     @Test(dependsOnMethods = {"testGetCookies"})
-    public void testGetWithCookies(){
-        //拼接测试的url
+    public void testGetWithCookies() throws IOException {
+        //创建get请求对象
         HttpGet get = new HttpGet(this.url+bundle.getString("test.get.with.cookies"));
-        CloseableHttpClient client = HttpClients.createDefault();
-
+        //创建CloseableHttpClient对象，设置Cookie + 创建response对象
+        CloseableHttpClient client = HttpClients.custom().setDefaultCookieStore(this.cookieStore).build();
+        CloseableHttpResponse response;
         //设置Cookie信息
+        response = client.execute(get);
+        //获取响应状态码
+        int statusCode = response.getStatusLine().getStatusCode();
+        //打印状态码
+        System.out.println("Code = "+ statusCode);
+        //判断状态码是否正确
+        if (statusCode == 200){
+            String result = EntityUtils.toString(response.getEntity(),"utf-8");
+            System.out.println(result);
+        }else{
+            System.out.println("状态ww码是否正确：false");
+        }
 
     }
 
